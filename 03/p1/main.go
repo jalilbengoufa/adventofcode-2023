5ce8d129e088281de0173244744cb9cbe72ed1a6
jalilbengoufa <jalilbengoufa@gmail.com>
2023-12-04 01:43:18 -0500

    solved 03 p1

---
+++ 03/p1/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"unicode"
+)
+
+type Point struct {
+	y int
+	x int
+}
+
+func main() {
+
+	filename := "data.txt"
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	result := 0
+
+	numbersMap := map[Point]string{}
+	symMap := []Point{}
+	index := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		currentN := "?"
+		for x, char := range line {
+			if unicode.IsDigit(char) {
+				if currentN == "?" {
+					currentN = string(char)
+				} else {
+
+					currentN += string(char)
+
+				}
+
+				if x == len(line)-1 && currentN != "?" {
+
+					numbersMap[Point{y: index, x: x}] = string(currentN)
+
+				}
+			} else if char != '.' {
+				symMap = append(symMap, Point{y: index, x: x})
+
+				if currentN != "?" {
+					numbersMap[Point{y: index, x: x - 1}] = string(currentN)
+					currentN = "?"
+				}
+			} else {
+
+				if currentN != "?" {
+					numbersMap[Point{y: index, x: x - 1}] = string(currentN)
+					currentN = "?"
+				}
+			}
+		}
+
+		index++
+	}
+
+	for point, number := range numbersMap {
+
+		nL := len(number)
+		isPartNumber := false
+
+		for _, sPoint := range symMap {
+
+			if sPoint.y == point.y {
+
+				if sPoint.x == point.x+1 || sPoint.x == point.x-nL {
+					isPartNumber = true
+					fmt.Println(number, point, "s")
+					break
+				}
+
+			}
+
+			if sPoint.y == point.y-1 {
+
+				if sPoint.x <= point.x+1 && sPoint.x >= point.x-nL {
+					isPartNumber = true
+					fmt.Println(number, point, "t")
+					break
+				}
+
+			}
+			if sPoint.y == point.y+1 {
+				if sPoint.x <= point.x+1 && sPoint.x >= point.x-nL {
+					isPartNumber = true
+					fmt.Println(number, point, "b")
+					break
+				}
+			}
+		}
+
+		if isPartNumber {
+			n, _ := strconv.Atoi(number)
+
+			result += n
+		}
+
+	}
+	fmt.Println("result", result)
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+}
